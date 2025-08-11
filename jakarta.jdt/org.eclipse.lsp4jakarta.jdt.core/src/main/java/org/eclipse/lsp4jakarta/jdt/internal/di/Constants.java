b37f0aee5858d8f3ff26912b2ba91db392cbcb9b
Rejoy J Kulangara <rejoy.kulangara@ibm.com>
2025-08-12 04:56:10 +0530

    Code Action to Remove Annotation Fails with Fully Qualified Annotation Names  (#569)
    * Test cases to address the issue #521
    * Code Action to Remove Annotation Fails with Fully Qualified Annotation Names #567
    * added fully qualified names in test to correct the behaviour
    * removed use of DiagnosticUtils.getSimpleName
    * updated quickfix logic to Constants.JSONB_TRANSIENT_FQ_NAME
    * updated test cases to fix issue #567
    * updated diagnostics participant to resolve #567
    * changed to fully qualified name to address issue #567
    * getting shortname for label #567
    * resolving fully qualified name from anotation
    * updating a test field to test the functionality
    * Update RemoveDynamicConstraintAnnotationQuickFix.java
    * removed unused method params
    * Removed unused shortName constants
    * pr comments addressed for #567
    * resolving type for only matching annotation to improve perfomance
    * Update RemoveAnnotationProposal.java
    * Update RemoveMethodParamAnnotationQuickFix.java

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/di/Constants.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/di/Constants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2021, 2022 IBM Corporation.
+* Copyright (c) 2021, 2025 IBM Corporation.
 *
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v. 2.0 which is available at
@@ -18,11 +18,7 @@ package org.eclipse.lsp4jakarta.jdt.internal.di;
 public class Constants {
 
     /* Annotation Constants */
-    public static final String PRODUCES = "Produces";
-    public static final String INJECT = "Inject";
     public static final String INJECT_FQ_NAME = "jakarta.inject.Inject";
-    public static final String QUALIFIER = "Qualifier";
-    public static final String NAMED = "Named";
 
     /* Diagnostics fields constants */
     public static final String DIAGNOSTIC_SOURCE = "jakarta-di";
