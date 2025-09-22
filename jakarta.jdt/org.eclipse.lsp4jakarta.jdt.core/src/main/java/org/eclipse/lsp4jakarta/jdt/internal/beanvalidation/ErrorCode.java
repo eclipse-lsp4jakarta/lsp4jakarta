ab30df7f4fd0a3e79692102794b193bb1088ca17
Rejoy <rejoy.kulangara@ibm.com>
2025-09-22 18:24:54 +0530

    Adding constants, error codes and messages

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/beanvalidation/ErrorCode.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/beanvalidation/ErrorCode.java
@@ -24,6 +24,7 @@ public enum ErrorCode implements IJavaErrorCode {
     InvalidAnnotationOnNonDateTimeMethodOrField,
     InvalidAnnotationOnNonMinMaxMethodOrField,
     InvalidAnnotationOnNonPositiveMethodOrField,
+    UseSizeOrNonEmptyAnnotationOnlyOnArrayCharSequenceCollectionOrMapType,
     InvalidAnnotationOnNonStringMethodOrField;
 
     /**
