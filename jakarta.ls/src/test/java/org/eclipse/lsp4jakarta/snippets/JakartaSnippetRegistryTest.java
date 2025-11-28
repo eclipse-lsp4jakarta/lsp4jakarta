d014d0ab88802e1f1e8d1158d6075995025b2d61
Rejoy <rejoy.kulangara@ibm.com>
2025-11-28 22:53:06 +0530

    code snippet for @MultipartConfig

--- jakarta.ls/src/test/java/org/eclipse/lsp4jakarta/snippets/JakartaSnippetRegistryTest.java
+++ jakarta.ls/src/test/java/org/eclipse/lsp4jakarta/snippets/JakartaSnippetRegistryTest.java
@@ -138,6 +138,10 @@ public class JakartaSnippetRegistryTest {
         assertTrue("servlet_webfilter Java snippet is not present in SnippetRegistry",
                    servletWebFilterSnippet.isPresent());
 
+        Optional<Snippet> servletMultipartConfigSnippet = findByPrefix("servlet_multipartconfig", registry);
+        assertTrue("servlet_multipartconfig Java snippet is not present in SnippetRegistry",
+                   servletMultipartConfigSnippet.isPresent());
+
         snippetsContextTest(servletGenericSnippet, "jakarta.servlet.GenericServlet",
                             JavaCursorContextKind.IN_EMPTY_FILE);
         snippetsContextTest(servletDoGetSnippet, "jakarta.servlet.http.HttpServlet",
@@ -145,6 +149,7 @@ public class JakartaSnippetRegistryTest {
         snippetsContextTest(servletDoPostSnippet, "jakarta.servlet.http.HttpServlet",
                             JavaCursorContextKind.IN_EMPTY_FILE);
         snippetsContextTest(servletWebFilterSnippet, "jakarta.servlet.Filter", JavaCursorContextKind.IN_EMPTY_FILE);
+        snippetsContextTest(servletMultipartConfigSnippet, "jakarta.servlet.http.HttpServlet", JavaCursorContextKind.IN_EMPTY_FILE);
 
     }
 
