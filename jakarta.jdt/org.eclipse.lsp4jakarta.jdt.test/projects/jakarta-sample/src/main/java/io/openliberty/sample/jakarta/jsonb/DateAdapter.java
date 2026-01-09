743e0797aedf07d489f2183ac6042170613ee2c6
Archana Iyer <archanr@ibm.com>
2026-01-09 15:19:10 +0530

    Comment added for a test class

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jsonb/DateAdapter.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jsonb/DateAdapter.java
@@ -3,6 +3,7 @@ package io.openliberty.sample.jakarta.jsonb;
 import jakarta.json.bind.adapter.JsonbAdapter;
 import java.util.Date;
 
+//Class used to test JSONB Annotation
 public class DateAdapter implements JsonbAdapter<Date, String> {
 	public String adaptToJson(Date date) {
 		return date.toString();
