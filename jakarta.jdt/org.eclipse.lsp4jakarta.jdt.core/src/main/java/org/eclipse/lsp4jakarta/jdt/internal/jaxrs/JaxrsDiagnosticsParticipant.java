cb2c2041888f3b02e56673623ee8cee3b196173a
Joseph-Bineesh <joseph.bineesh@ibm.com>
2026-02-05 12:37:41 +0530

    Format source code

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/jaxrs/JaxrsDiagnosticsParticipant.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/jaxrs/JaxrsDiagnosticsParticipant.java
@@ -29,101 +29,95 @@ import org.eclipse.lsp4jakarta.jdt.internal.core.ls.JDTUtilsLSImpl;
 
 public class JaxrsDiagnosticsParticipant implements IJavaDiagnosticsParticipant {
 
-	private static final Logger LOGGER = Logger.getLogger(JaxrsDiagnosticsParticipant.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(JaxrsDiagnosticsParticipant.class.getName());
 
-	@Override
-	public List<Diagnostic> collectDiagnostics(JavaDiagnosticsContext context, IProgressMonitor monitor)
-			throws CoreException {
-		String uri = context.getUri();
-		IJDTUtils utils = JDTUtilsLSImpl.getInstance();
-		ICompilationUnit unit = utils.resolveCompilationUnit(uri);
-		List<Diagnostic> diagnostics = new ArrayList<>();
+    @Override
+    public List<Diagnostic> collectDiagnostics(JavaDiagnosticsContext context, IProgressMonitor monitor) throws CoreException {
+        String uri = context.getUri();
+        IJDTUtils utils = JDTUtilsLSImpl.getInstance();
+        ICompilationUnit unit = utils.resolveCompilationUnit(uri);
+        List<Diagnostic> diagnostics = new ArrayList<>();
 
-		if (unit == null) {
-			return diagnostics;
-		}
+        if (unit == null) {
+            return diagnostics;
+        }
 
-		IType[] alltypes = unit.getAllTypes();
-		IMethod[] methods;
+        IType[] alltypes = unit.getAllTypes();
+        IMethod[] methods;
 
-		for (IType type : alltypes) {
-			if (!type.isClass()) {
-				continue;
-			}
+        for (IType type : alltypes) {
+            if (!type.isClass()) {
+                continue;
+            }
 
-			boolean isJaxrsClass = false;
-			// Iterate class level annotations
-			for (IAnnotation annotation : type.getAnnotations()) {
-				isJaxrsClass = DiagnosticUtils.isMatchedJavaElement(type, annotation.getElementName(),
-						Constants.PATH_ANNOTATION);
-			}
+            boolean isJaxrsClass = false;
+            // Iterate class level annotations
+            for (IAnnotation annotation : type.getAnnotations()) {
+                isJaxrsClass = DiagnosticUtils.isMatchedJavaElement(type, annotation.getElementName(),
+                                                                    Constants.PATH_ANNOTATION);
+            }
 
-			if (isJaxrsClass) {
-				methods = type.getMethods();
-				for (IMethod method : methods) {
-					if (DiagnosticUtils.isConstructorMethod(method) || validateSetterMethod(method)) {
-						for (ILocalVariable param : method.getParameters()) {
-							Stream.of(param.getAnnotations())
-						    .filter(paramAnnotation -> {
-								try {
-									return isConstraintAnnotation(paramAnnotation, type, unit);
-								} catch (JavaModelException e) {			
-									return false;
-								}
-							})
-						    .map(paramAnnotation -> {
-								try {
-									return PositionUtils.toNameRange(paramAnnotation, context.getUtils());
-								} catch (JavaModelException e) {
-									return null;
-								}
-							})
-						    .map(annotationRange -> context.createDiagnostic(uri,
-						        Messages.getMessage("InvalidConstraintTarget"),
-						        annotationRange, 
-						        Constants.DIAGNOSTIC_SOURCE, 
-						        ErrorCode.InvalidConstraintTarget,
-						        DiagnosticSeverity.Error))
-						    .forEach(diagnostics::add);
-						}
-					}
-				}
-			}
-		}
+            if (isJaxrsClass) {
+                methods = type.getMethods();
+                for (IMethod method : methods) {
+                    if (DiagnosticUtils.isConstructorMethod(method) || validateSetterMethod(method)) {
+                        for (ILocalVariable param : method.getParameters()) {
+                            Stream.of(param.getAnnotations()).filter(paramAnnotation -> {
+                                try {
+                                    return isConstraintAnnotation(paramAnnotation, type, unit);
+                                } catch (JavaModelException e) {
+                                    return false;
+                                }
+                            }).map(paramAnnotation -> {
+                                try {
+                                    return PositionUtils.toNameRange(paramAnnotation, context.getUtils());
+                                } catch (JavaModelException e) {
+                                    return null;
+                                }
+                            }).map(annotationRange -> context.createDiagnostic(uri,
+                                                                               Messages.getMessage("InvalidConstraintTarget"),
+                                                                               annotationRange,
+                                                                               Constants.DIAGNOSTIC_SOURCE,
+                                                                               ErrorCode.InvalidConstraintTarget,
+                                                                               DiagnosticSeverity.Error)).forEach(diagnostics::add);
+                        }
+                    }
+                }
+            }
+        }
 
-		return diagnostics;
+        return diagnostics;
 
-	}
+    }
 
-	private boolean isConstraintAnnotation(IAnnotation annotation, IType type, ICompilationUnit cu)
-			throws JavaModelException {
-		boolean isConstraint = false;
-		String annotationFQ = ManagedBean.getFullyQualifiedClassName(type, annotation.getElementName());
-		IJavaProject project = annotation.getJavaProject();
+    private boolean isConstraintAnnotation(IAnnotation annotation, IType type, ICompilationUnit cu) throws JavaModelException {
+        boolean isConstraint = false;
+        String annotationFQ = ManagedBean.getFullyQualifiedClassName(type, annotation.getElementName());
+        IJavaProject project = annotation.getJavaProject();
 
-		if (project != null && annotationFQ != null) {
-			IType annotationType = project.findType(annotationFQ);
-			if (annotationType != null) {
-				isConstraint = Arrays.stream(annotationType.getAnnotations()).anyMatch(constraintAnnotation -> {
-					try {
-						return DiagnosticUtils.isMatchedAnnotation(cu, constraintAnnotation,
-								Constants.CONSTRAINT_ANNOTATION);
-					} catch (JavaModelException e) {
-						LOGGER.log(Level.INFO, "Unable to fetch constraint information", e.getMessage());
-						return false;
-					}
-				});
+        if (project != null && annotationFQ != null) {
+            IType annotationType = project.findType(annotationFQ);
+            if (annotationType != null) {
+                isConstraint = Arrays.stream(annotationType.getAnnotations()).anyMatch(constraintAnnotation -> {
+                    try {
+                        return DiagnosticUtils.isMatchedAnnotation(cu, constraintAnnotation,
+                                                                   Constants.CONSTRAINT_ANNOTATION);
+                    } catch (JavaModelException e) {
+                        LOGGER.log(Level.INFO, "Unable to fetch constraint information", e.getMessage());
+                        return false;
+                    }
+                });
 
-			}
-		}
-		return isConstraint;
-	}
+            }
+        }
+        return isConstraint;
+    }
 
-	private boolean validateSetterMethod(IMethod method) throws JavaModelException {
-		if (!method.getElementName().startsWith("set") || !"V".equalsIgnoreCase(method.getReturnType()) || method.getParameterTypes().length != 1) {
-			return false;
-		}
-		return true;
-	}
+    private boolean validateSetterMethod(IMethod method) throws JavaModelException {
+        if (!method.getElementName().startsWith("set") || !"V".equalsIgnoreCase(method.getReturnType()) || method.getParameterTypes().length != 1) {
+            return false;
+        }
+        return true;
+    }
 
 }
--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jaxrs/NoPublicConstructorClass.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jaxrs/NoPublicConstructorClass.java
@@ -1,25 +1,16 @@
 package io.openliberty.sample.jakarta.jaxrs;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.Path;
 
 @Path("/somewhere")
 public class NoPublicConstructorClass {
 
-    String name;
-    
-
-    public NoPublicConstructorClass(String name,JB jb) {
-	}
-
-	protected NoPublicConstructorClass(int arg1) {
+    private NoPublicConstructorClass() {
 
     }
-	
-	public void setname(@BeanParam JB jb) {
-		
-	}
+
+    protected NoPublicConstructorClass(int arg1) {
+
+    }
 
 }
