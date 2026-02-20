96ca9221de65ac6a9afb6a4c9eed25696f14b162
Archana Iyer <archanr@ibm.com>
2026-02-20 13:05:32 +0530

    Added new constants and Error code files for interceptor

---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/interceptor/Constants.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *		IBM Corporation, Archana Iyer R - initial implementation
+ *******************************************************************************/
+package org.eclipse.lsp4jakarta.jdt.internal.interceptor;
+
+/**
+ * Interceptor constants.
+ */
+public class Constants {
+
+    /* INTERCEPTOR_FQ_NAME */
+    public static final String INTERCEPTOR_FQ_NAME = "jakarta.interceptor.Interceptor";
+
+    /* Source */
+    public static final String DIAGNOSTIC_SOURCE = "jakarta-interceptor";
+
+}
---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/interceptor/ErrorCode.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+* Copyright (c) 2026 IBM Corporation and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*     IBM Corporation, Archana Iyer R - initial implementation
+*******************************************************************************/
+package org.eclipse.lsp4jakarta.jdt.internal.interceptor;
+
+import org.eclipse.lsp4jakarta.jdt.core.java.diagnostics.IJavaErrorCode;
+
+/**
+ * Interceptor error codes.
+ */
+public enum ErrorCode implements IJavaErrorCode {
+    InvalidInterceptorNoArgsConstructorMissing,
+    InvalidInterceptorAnnotationOnAbstractClass;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCode() {
+        return name();
+    }
+}
