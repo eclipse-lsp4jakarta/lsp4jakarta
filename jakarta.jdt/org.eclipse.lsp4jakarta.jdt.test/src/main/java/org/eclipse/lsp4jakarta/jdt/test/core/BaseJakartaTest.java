5a7852f880163e254f61c68bf65a09ade374c102
Joseph-Bineesh <joseph.bineesh@ibm.com>
2026-03-11 12:37:13 +0530

    Removed unused imports

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/src/main/java/org/eclipse/lsp4jakarta/jdt/test/core/BaseJakartaTest.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/src/main/java/org/eclipse/lsp4jakarta/jdt/test/core/BaseJakartaTest.java
@@ -27,11 +27,9 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.core.JavaModelManager;
-import org.eclipse.jdt.launching.JavaRuntime;
 
 /**
  * Modified from:
