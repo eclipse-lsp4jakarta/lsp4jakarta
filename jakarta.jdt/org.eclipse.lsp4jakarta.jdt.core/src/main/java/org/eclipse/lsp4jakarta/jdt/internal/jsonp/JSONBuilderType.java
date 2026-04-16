c9d9f875fece90076a05646fe406f0c7a96c61d8
Archana Iyer <archanr@ibm.com>
2026-04-16 16:01:59 +0530

    Added documentation

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/jsonp/JSONBuilderType.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/jsonp/JSONBuilderType.java
@@ -1,7 +1,26 @@
 package org.eclipse.lsp4jakarta.jdt.internal.jsonp;
 
+/**
+*
+* This enum is used to indicate whether a JSON builder should
+* construct an object, an array, or handle an unknown type.
+* 
+*/
 public enum JSONBuilderType {
+	
+	/**
+     * Indicates that the JSON builder should create a JSON object.
+     */
     OBJECT,
+    
+    /**
+     * Indicates that the JSON builder should create a JSON array.
+     */
     ARRAY,
+    
+    /**
+     * Indicates that the JSON type is unknown or cannot be determined.
+     * This can be used as a default or fallback value.
+     */
     UNKNOWN
 }
