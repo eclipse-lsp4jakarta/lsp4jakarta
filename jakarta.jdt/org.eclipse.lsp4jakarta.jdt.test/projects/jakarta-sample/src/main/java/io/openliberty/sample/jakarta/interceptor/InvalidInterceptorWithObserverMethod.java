89d55c320070447b5a72035780f028e49d415047
Rejoy <rejoy.kulangara@ibm.com>
2026-05-19 17:52:18 +0530

    removed the system.out

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/interceptor/InvalidInterceptorWithObserverMethod.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/interceptor/InvalidInterceptorWithObserverMethod.java
@@ -12,23 +12,21 @@ public class InvalidInterceptorWithObserverMethod {
 
     // Invalid: Interceptor with @Observes parameter
     public void observerMethod(@Observes String event) {
-        System.out.println("Observer method: " + event);
+
     }
 
     // Invalid: Interceptor with @ObservesAsync parameter
     public void observerAsyncMethod(@ObservesAsync String event) {
-        System.out.println("Observer async method: " + event);
+
     }
 
     // Invalid: Interceptor with both @Observes and @ObservesAsync parameters
     public void observerBothMethod(@Observes String event1, @ObservesAsync String event2) {
-        System.out.println("Observer both method");
+
     }
 
     // Valid: Regular method without observer annotations
     public void regularMethod(String param) {
-        System.out.println("Regular method: " + param);
+
     }
 }
-
-// Made with Bob
