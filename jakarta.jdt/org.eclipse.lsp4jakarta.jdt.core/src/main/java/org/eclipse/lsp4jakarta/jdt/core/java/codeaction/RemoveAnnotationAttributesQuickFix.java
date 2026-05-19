ea24c6cd89231bd47512cfc58ae6aac8e89e8ec7
Rejoy <rejoy.kulangara@ibm.com>
2026-05-19 17:07:31 +0530

    further optimized the code

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/java/codeaction/RemoveAnnotationAttributesQuickFix.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/java/codeaction/RemoveAnnotationAttributesQuickFix.java
@@ -40,7 +40,6 @@ import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4jakarta.commons.codeaction.CodeActionResolveData;
 import org.eclipse.lsp4jakarta.commons.codeaction.ICodeActionId;
 import org.eclipse.lsp4jakarta.jdt.core.ASTNodeUtils;
-import org.eclipse.lsp4jakarta.jdt.core.java.corrections.proposal.ChangeCorrectionProposal;
 import org.eclipse.lsp4jakarta.jdt.core.java.corrections.proposal.ModifyAnnotationProposal;
 
 /**
@@ -96,11 +95,7 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
     @Override
     public List<? extends CodeAction> getCodeActions(JavaCodeActionContext context, Diagnostic diagnostic,
                                                      IProgressMonitor monitor) throws CoreException {
-        if (isParameterLevel) {
-            return getParameterLevelCodeActions(context, diagnostic);
-        } else {
-            return getFieldMethodLevelCodeActions(context, diagnostic);
-        }
+        return isParameterLevel ? getParameterLevelCodeActions(context, diagnostic) : getFieldMethodLevelCodeActions(context, diagnostic);
     }
 
     /**
@@ -111,18 +106,13 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
         List<CodeAction> codeActions = new ArrayList<>();
         ASTNode node = context.getCoveredNode();
         MethodDeclaration parentNode = (MethodDeclaration) node.getParent();
-        List<SingleVariableDeclaration> parameters = (List<SingleVariableDeclaration>) parentNode.parameters();
+        List<SingleVariableDeclaration> parameters = parentNode.parameters();
 
         for (SingleVariableDeclaration parameter : parameters) {
-            // Collect all matching annotations that have attributes to remove
-            List<String> annotationsToProcess = findAnnotationsWithAttributesToRemove(parameter);
-
-            // Create a code action for each annotation
-            for (String annotation : annotationsToProcess) {
-                createParameterCodeAction(diagnostic, context, codeActions, parameter, annotation);
+            for (String annotation : findAnnotationsWithAttributesToRemove(parameter)) {
+                createCodeAction(diagnostic, context, codeActions, parameter, annotation);
             }
         }
-
         return codeActions;
     }
 
@@ -130,51 +120,34 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
      * Creates code actions for field/method-level annotations.
      */
     private List<? extends CodeAction> getFieldMethodLevelCodeActions(JavaCodeActionContext context, Diagnostic diagnostic) {
-        ExtendedCodeAction codeAction = new ExtendedCodeAction(getLabel());
-        codeAction.setRelevance(0);
-        codeAction.setKind(CodeActionKind.QuickFix);
-        codeAction.setDiagnostics(Arrays.asList(diagnostic));
-        codeAction.setData(new CodeActionResolveData(context.getUri(), getParticipantId(), context.getParams().getRange(), null, context.getParams().isResourceOperationSupported(), context.getParams().isCommandConfigurationUpdateSupported(), getCodeActionId()));
-        return Collections.singletonList(codeAction);
+        return Collections.singletonList(createCodeAction(getLabel(), diagnostic, context, null));
     }
 
     @Override
     public CodeAction resolveCodeAction(JavaCodeActionResolveContext context) {
-        if (isParameterLevel) {
-            return resolveParameterLevelCodeAction(context);
-        } else {
-            return resolveFieldMethodLevelCodeAction(context);
-        }
+        return isParameterLevel ? resolveParameterLevelCodeAction(context) : resolveFieldMethodLevelCodeAction(context);
     }
 
     /**
      * Resolves code action for parameter-level annotations.
      */
+    @SuppressWarnings("unchecked")
     private CodeAction resolveParameterLevelCodeAction(JavaCodeActionResolveContext context) {
         CodeAction toResolve = context.getUnresolved();
-        ASTNode node = context.getCoveredNode();
-        MethodDeclaration parentNode = (MethodDeclaration) node.getParent();
-
         CodeActionResolveData data = (CodeActionResolveData) toResolve.getData();
+
         String paramName = (String) data.getExtendedDataEntry(PARAMETER_NAME_KEY);
         List<String> annotationsList = (List<String>) data.getExtendedDataEntry(ANNOTATIONS_KEY);
         List<String> attributesList = (List<String>) data.getExtendedDataEntry(ATTRIBUTES_KEY);
 
-        String[] annotationsArray = annotationsList.toArray(new String[0]);
-
-        IBinding binding = getParameterBinding(parentNode, paramName, annotationsArray);
+        MethodDeclaration parentNode = (MethodDeclaration) context.getCoveredNode().getParent();
+        IBinding binding = getParameterBinding(parentNode, paramName, annotationsList.toArray(new String[0]));
 
         if (binding != null) {
-            String label = getLabel(annotationsArray[0], attributesList.toArray(new String[0]));
-            ChangeCorrectionProposal proposal = new ModifyAnnotationProposal(label, context.getCompilationUnit(), context.getASTRoot(), binding, 0, annotationsArray[0], new ArrayList<String>(), attributesList);
-
-            try {
-                toResolve.setEdit(context.convertToWorkspaceEdit(proposal));
-            } catch (CoreException e) {
-                LOGGER.log(Level.SEVERE, "Unable to resolve code action to remove annotation attributes", e);
-            }
+            String label = getLabel(annotationsList.get(0), attributesList.toArray(new String[0]));
+            ModifyAnnotationProposal proposal = new ModifyAnnotationProposal(label, context.getCompilationUnit(), context.getASTRoot(), binding, 0, annotationsList.get(0), Collections.emptyList(), attributesList);
+            setWorkspaceEdit(toResolve, context, proposal);
         }
-
         return toResolve;
     }
 
@@ -183,29 +156,34 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
      */
     private CodeAction resolveFieldMethodLevelCodeAction(JavaCodeActionResolveContext context) {
         CodeAction toResolve = context.getUnresolved();
-        ASTNode selectedNode = context.getCoveringNode();
-        IBinding parentType = getBinding(selectedNode);
-        ChangeCorrectionProposal proposal = new ModifyAnnotationProposal(getLabel(), context.getCompilationUnit(), context.getASTRoot(), parentType, 0, annotations[0], new ArrayList<String>(), attributeNames);
-        try {
-            toResolve.setEdit(context.convertToWorkspaceEdit(proposal));
-        } catch (CoreException e) {
-            LOGGER.log(Level.SEVERE, "Unable to resolve code action edit for removing an attribute value", e);
-        }
+        IBinding parentType = getBinding(context.getCoveringNode());
+        ModifyAnnotationProposal proposal = new ModifyAnnotationProposal(getLabel(), context.getCompilationUnit(), context.getASTRoot(), parentType, 0, annotations[0], Collections.emptyList(), attributeNames);
+        setWorkspaceEdit(toResolve, context, proposal);
         return toResolve;
+    }
+
+    /**
+     * Sets the workspace edit on the code action from the proposal.
+     */
+    private void setWorkspaceEdit(CodeAction codeAction, JavaCodeActionResolveContext context, ModifyAnnotationProposal proposal) {
+        try {
+            codeAction.setEdit(context.convertToWorkspaceEdit(proposal));
+        } catch (CoreException e) {
+            LOGGER.log(Level.SEVERE, "Unable to resolve code action to remove annotation attributes", e);
+        }
     }
 
     /**
      * Gets the binding for field/method-level annotations.
      */
+    @SuppressWarnings("restriction")
     protected IBinding getBinding(ASTNode node) {
         ASTNode parent = node.getParent();
 
-        // Method annotation
         if (parent instanceof MethodDeclaration) {
             return ((MethodDeclaration) parent).resolveBinding();
         }
 
-        // Field annotation - get VariableDeclarationFragment from FieldDeclaration
         if (parent instanceof FieldDeclaration) {
             FieldDeclaration fieldDecl = (FieldDeclaration) parent;
             if (!fieldDecl.fragments().isEmpty()) {
@@ -222,7 +200,6 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
      */
     private List<String> findAnnotationsWithAttributesToRemove(SingleVariableDeclaration parameter) {
         List<String> result = new ArrayList<>();
-
         for (Annotation annotation : getParameterAnnotations(parameter)) {
             ITypeBinding typeBinding = annotation.resolveTypeBinding();
             if (typeBinding != null && isTargetAnnotation(typeBinding.getQualifiedName())
@@ -238,60 +215,65 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
      */
     @SuppressWarnings("unchecked")
     private List<Annotation> getParameterAnnotations(SingleVariableDeclaration parameter) {
-        List<Annotation> annotations = new ArrayList<>();
-        List<ASTNode> modifiers = (List<ASTNode>) parameter.getStructuralProperty(
-                                                                                  SingleVariableDeclaration.MODIFIERS2_PROPERTY);
-
+        List<Annotation> result = new ArrayList<>();
+        List<ASTNode> modifiers = (List<ASTNode>) parameter.getStructuralProperty(SingleVariableDeclaration.MODIFIERS2_PROPERTY);
         for (ASTNode modifier : modifiers) {
             if (ASTNodeUtils.isAnnotation(modifier)) {
-                annotations.add((Annotation) modifier);
+                result.add((Annotation) modifier);
             }
         }
-        return annotations;
+        return result;
     }
 
     /**
      * Checks if the given annotation name matches any of the target annotations.
      */
     private boolean isTargetAnnotation(String annotationName) {
-        return Arrays.asList(this.annotations).contains(annotationName);
+        for (String annotation : annotations) {
+            if (annotation.equals(annotationName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Creates a code action with the given parameters.
+     */
+    private ExtendedCodeAction createCodeAction(String label, Diagnostic diagnostic, JavaCodeActionContext context,
+                                                Map<String, Object> extendedData) {
+        ExtendedCodeAction codeAction = new ExtendedCodeAction(label);
+        codeAction.setRelevance(0);
+        codeAction.setKind(CodeActionKind.QuickFix);
+        codeAction.setDiagnostics(Collections.singletonList(diagnostic));
+        codeAction.setData(new CodeActionResolveData(context.getUri(), getParticipantId(), context.getParams().getRange(), extendedData, context.getParams().isResourceOperationSupported(), context.getParams().isCommandConfigurationUpdateSupported(), getCodeActionId()));
+        return codeAction;
     }
 
     /**
      * Creates a code action for removing attributes from an annotation on a parameter.
      */
-    protected void createParameterCodeAction(Diagnostic diagnostic, JavaCodeActionContext context,
-                                             List<CodeAction> codeActions, SingleVariableDeclaration parameter,
-                                             String annotation) {
+    private void createCodeAction(Diagnostic diagnostic, JavaCodeActionContext context,
+                                  List<CodeAction> codeActions, SingleVariableDeclaration parameter,
+                                  String annotation) {
         String label = getLabel(annotation, attributeNames.toArray(new String[0]));
-
-        ExtendedCodeAction codeAction = new ExtendedCodeAction(label);
-        codeAction.setRelevance(0);
-        codeAction.setKind(CodeActionKind.QuickFix);
-        codeAction.setDiagnostics(Arrays.asList(diagnostic));
-
         Map<String, Object> extendedData = new HashMap<>();
         extendedData.put(PARAMETER_NAME_KEY, parameter.getName().getIdentifier());
-        extendedData.put(ANNOTATIONS_KEY, Arrays.asList(annotation));
+        extendedData.put(ANNOTATIONS_KEY, Collections.singletonList(annotation));
         extendedData.put(ATTRIBUTES_KEY, attributeNames);
-
-        codeAction.setData(new CodeActionResolveData(context.getUri(), getParticipantId(), context.getParams().getRange(), extendedData, context.getParams().isResourceOperationSupported(), context.getParams().isCommandConfigurationUpdateSupported(), getCodeActionId()));
-
-        codeActions.add(codeAction);
+        codeActions.add(createCodeAction(label, diagnostic, context, extendedData));
     }
 
     /**
      * Gets the binding for a parameter by name and annotation.
      */
     @SuppressWarnings("unchecked")
-    protected IBinding getParameterBinding(MethodDeclaration method, String paramName, String[] annotations) {
-        List<SingleVariableDeclaration> parameters = method.parameters();
-
-        for (SingleVariableDeclaration param : parameters) {
+    private IBinding getParameterBinding(MethodDeclaration method, String paramName, String[] targetAnnotations) {
+        for (SingleVariableDeclaration param : (List<SingleVariableDeclaration>) method.parameters()) {
             if (param.getName().getIdentifier().equals(paramName)) {
                 for (Annotation annotation : getParameterAnnotations(param)) {
                     ITypeBinding typeBinding = annotation.resolveTypeBinding();
-                    if (typeBinding != null && Arrays.asList(annotations).contains(typeBinding.getQualifiedName())) {
+                    if (typeBinding != null && isTargetAnnotation(typeBinding.getQualifiedName())) {
                         return param.resolveBinding();
                     }
                 }
@@ -308,14 +290,11 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
     @SuppressWarnings("unchecked")
     protected boolean hasAttributesToRemove(Annotation annotation) {
         if (!(annotation instanceof NormalAnnotation)) {
-            return false; // Only NormalAnnotations have name-value pairs
+            return false;
         }
 
-        NormalAnnotation normalAnnotation = (NormalAnnotation) annotation;
-        List<MemberValuePair> values = normalAnnotation.values();
-
-        // Check if any target attribute exists and should be removed
-        for (String attributeToRemove : this.attributeNames) {
+        List<MemberValuePair> values = ((NormalAnnotation) annotation).values();
+        for (String attributeToRemove : attributeNames) {
             for (MemberValuePair mvp : values) {
                 if (mvp.getName().getFullyQualifiedName().equals(attributeToRemove)
                     && shouldRemoveAttribute(mvp)) {
@@ -344,7 +323,6 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
      * @return The label for the code action
      */
     protected String getLabel() {
-        // For parameter-level, this won't be called directly
         return getLabel(annotations[0], attributeNames.toArray(new String[0]));
     }
 
@@ -357,11 +335,8 @@ public abstract class RemoveAnnotationAttributesQuickFix implements IJavaCodeAct
      * @return The label for the code action
      */
     protected String getLabel(String annotation, String[] attributes) {
-        // Default implementation for field/method-level
         return getLabel();
     }
 
     protected abstract ICodeActionId getCodeActionId();
 }
-
-// Made with Bob
