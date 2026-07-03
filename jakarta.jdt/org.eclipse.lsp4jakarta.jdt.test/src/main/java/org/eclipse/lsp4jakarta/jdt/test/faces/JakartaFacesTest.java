6756be8e8f6af20b914d0f116f8cddd75aa6233b
Sajeer <sajeer.babu@ibm.com>
2026-07-03 15:06:22 +0530

    Added diagnostic for @FacesValidator classes that do not implement jakarta.faces.validator.Validator

---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/src/main/java/org/eclipse/lsp4jakarta/jdt/test/faces/JakartaFacesTest.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+* Copyright (c) 2026 IBM Corporation and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*     IBM Corporation - initial API and implementation
+*******************************************************************************/
+
+package org.eclipse.lsp4jakarta.jdt.test.faces;
+
+import static org.eclipse.lsp4jakarta.jdt.test.core.JakartaForJavaAssert.assertJavaDiagnostics;
+import static org.eclipse.lsp4jakarta.jdt.test.core.JakartaForJavaAssert.d;
+
+import java.util.Arrays;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4jakarta.commons.JakartaJavaDiagnosticsParams;
+import org.eclipse.lsp4jakarta.jdt.core.utils.IJDTUtils;
+import org.eclipse.lsp4jakarta.jdt.internal.core.ls.JDTUtilsLSImpl;
+import org.eclipse.lsp4jakarta.jdt.test.core.BaseJakartaTest;
+import org.junit.Test;
+
+public class JakartaFacesTest extends BaseJakartaTest {
+
+    protected static IJDTUtils IJDT_UTILS = JDTUtilsLSImpl.getInstance();
+
+    @Test
+    public void implementFacesValidator() throws Exception {
+        IJavaProject javaProject = loadJavaProject("jakarta-sample", "");
+        IFile javaFile = javaProject.getProject().getFile(
+                                                          new Path("src/main/java/io/openliberty/sample/jakarta/faces/DontImplementValidator.java"));
+        String uri = javaFile.getLocation().toFile().toURI().toString();
+
+        JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
+        diagnosticsParams.setUris(Arrays.asList(uri));
+
+        // expected: diagnostic on class name (line 5, col 13..35)
+        Diagnostic d = d(5, 13, 35,
+                         "Annotated classes with @FacesValidator must implement the jakarta.faces.validator.Validator interface.",
+                         DiagnosticSeverity.Error, "jakarta-faces",
+                         "FacesValidatorAnnotatedClassNoValidatorInterfaceImpl");
+
+        assertJavaDiagnostics(diagnosticsParams, IJDT_UTILS, d);
+    }
+
+    @Test
+    public void facesValidatorWithValidatorInterface() throws Exception {
+        IJavaProject javaProject = loadJavaProject("jakarta-sample", "");
+        IFile javaFile = javaProject.getProject().getFile(
+                                                          new Path("src/main/java/io/openliberty/sample/jakarta/faces/ImplementsValidator.java"));
+        String uri = javaFile.getLocation().toFile().toURI().toString();
+
+        JakartaJavaDiagnosticsParams diagnosticsParams = new JakartaJavaDiagnosticsParams();
+        diagnosticsParams.setUris(Arrays.asList(uri));
+
+        // expected: no diagnostics for a compliant class
+        assertJavaDiagnostics(diagnosticsParams, IJDT_UTILS);
+    }
+}
