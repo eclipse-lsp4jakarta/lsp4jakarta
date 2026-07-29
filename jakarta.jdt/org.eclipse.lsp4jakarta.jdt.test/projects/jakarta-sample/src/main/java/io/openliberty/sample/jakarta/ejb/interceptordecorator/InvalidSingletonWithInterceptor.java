bef2fc62c477c90ba76ed72a7275ce10325c6818
Archana Iyer <archanr@ibm.com>
2026-07-29 16:13:31 +0530

    Build fix for regression junit failures

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/InvalidSingletonWithInterceptor.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/InvalidSingletonWithInterceptor.java
@@ -1,11 +1,13 @@
 package io.openliberty.sample.jakarta.ejb.interceptordecorator;
 
+import io.openliberty.sample.jakarta.interceptor.Monitored;
 import jakarta.ejb.Singleton;
 import jakarta.interceptor.Interceptor;
 
 // Invalid: @Singleton with @Interceptor
 @Singleton
 @Interceptor
+@Monitored
 class InvalidSingletonWithInterceptor {
     public void businessMethod() {
     }
--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/InvalidStatelessWithInterceptor.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/InvalidStatelessWithInterceptor.java
@@ -1,11 +1,13 @@
 package io.openliberty.sample.jakarta.ejb.interceptordecorator;
 
+import io.openliberty.sample.jakarta.interceptor.Monitored;
 import jakarta.ejb.Stateless;
 import jakarta.interceptor.Interceptor;
 
 // Invalid: @Stateless with @Interceptor
 @Stateless
 @Interceptor
+@Monitored
 class InvalidStatelessWithInterceptor {
     public void businessMethod() {
     }
