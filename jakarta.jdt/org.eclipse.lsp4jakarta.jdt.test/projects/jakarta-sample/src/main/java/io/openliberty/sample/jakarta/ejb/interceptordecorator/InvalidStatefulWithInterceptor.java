2c0443d07b6a300ac3788100605268e3596ed9ec
Archana Iyer <archanr@ibm.com>
2026-07-29 18:59:37 +0530

    Build fix for regression junit failures (#945)

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/InvalidStatefulWithInterceptor.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/InvalidStatefulWithInterceptor.java
@@ -1,11 +1,13 @@
 package io.openliberty.sample.jakarta.ejb.interceptordecorator;
 
+import io.openliberty.sample.jakarta.interceptor.Monitored;
 import jakarta.ejb.Stateful;
 import jakarta.interceptor.Interceptor;
 
 // Invalid: @Stateful with @Interceptor
 @Stateful
 @Interceptor
+@Monitored
 class InvalidStatefulWithInterceptor {
     public void businessMethod() {
     }
--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/ValidInterceptor.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/ejb/interceptordecorator/ValidInterceptor.java
@@ -1,9 +1,11 @@
 package io.openliberty.sample.jakarta.ejb.interceptordecorator;
 
+import io.openliberty.sample.jakarta.interceptor.Monitored;
 import jakarta.interceptor.Interceptor;
 
 // Valid: @Interceptor without session bean annotation
 @Interceptor
+@Monitored
 class ValidInterceptor {
     public void intercept() {
     }
