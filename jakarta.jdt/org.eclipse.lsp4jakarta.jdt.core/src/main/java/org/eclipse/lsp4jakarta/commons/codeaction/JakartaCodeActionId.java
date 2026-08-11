fa36f5e00f91dd9b359e18217b2940930c671647
Rejoy J Kulangara <rejoy.kulangara@ibm.com>
2026-08-11 11:18:18 +0530

    Sessionbean public nonfinal nonabstract toplevel (#921)
    * initial commit
    
    * added tests
    
    * All classes have been moved to individual files.
    
    * Added remove session bean quickfix
    
    * splited the classes
    
    * Update EjbDiagnosticsParticipant.java
    
    * removed the duplicate quickfixes
    
    * pr comment
    
    * removed number doc
    
    * updated to meaningful variables
    
    * updated the meaningful variables
    
    * conflicts resolved

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/commons/codeaction/JakartaCodeActionId.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/commons/codeaction/JakartaCodeActionId.java
@@ -93,8 +93,6 @@ public enum JakartaCodeActionId implements ICodeActionId {
     CDIRemoveConditionalObserverAnnotations,
     CDIRemoveNotifyObserverAttribute,
     CDIRemoveObserverConflictParams,
-    CDIRemoveSingletonAnnotation,
-    CDIRemoveStatelessAnnotation,
     CDIReplaceInvalidScopesWithDependent,
     CDIRemoveDisposesFromInterceptorDecorator,
     CDIRemoveNamedAnnotation,
---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/cdi/sessionbean/StatelessWithSessionScoped.java
@@ -0,0 +1,11 @@
+package io.openliberty.sample.jakarta.cdi.sessionbean;
+
+import jakarta.ejb.Stateless;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.context.SessionScoped;
+
+// Invalid: Stateless with SessionScoped
+@Stateless
+@SessionScoped
+public class StatelessWithSessionScoped {
+}
