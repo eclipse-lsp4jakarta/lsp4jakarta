3236a2dd590cc94debb2ca3ff2f8ff0b6bb64f6b
Rejoy <rejoy.kulangara@ibm.com>
2026-08-11 17:21:03 +0530

    Update TypeHierarchyUtils.java

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/utils/TypeHierarchyUtils.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/utils/TypeHierarchyUtils.java
@@ -19,6 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
@@ -167,6 +168,18 @@ public class TypeHierarchyUtils {
      */
     public static boolean inheritsFrom(IType fieldType, String superType) throws CoreException {
         return doesITypeHaveSuperType(fieldType, superType) == HAS_SUPERTYPE;
+    }
+
+    /**
+     * Gets all interfaces implemented by a type and its superclasses.
+     *
+     * @param type the type to inspect
+     * @return an array of all interface types
+     * @throws JavaModelException if an error occurs accessing the Java model
+     */
+    public static IType[] getAllInterfaces(IType type) throws JavaModelException {
+        ITypeHierarchy typeHierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
+        return typeHierarchy.getAllInterfaces();
     }
 
     /**
