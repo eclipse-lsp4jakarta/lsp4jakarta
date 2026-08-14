bcc950a5ba069634eeabf0934c6aa801e0afadd3
Rejoy J Kulangara <rejoy.kulangara@ibm.com>
2026-08-14 11:42:53 +0530

    Invalid session sync method (#929)
    * initial commit
    
    * tests
    
    * created a common quick fix class
    
    * pr comments
    
    * added this inside if condition
    
    * added negative tests

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/DiagnosticUtils.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/DiagnosticUtils.java
@@ -525,4 +525,19 @@ public class DiagnosticUtils {
     public static String[] getAnnotationNames(IMethod method) throws JavaModelException {
         return Stream.of(method.getAnnotations()).map(annotation -> annotation.getElementName()).toArray(String[]::new);
     }
+
+    /**
+     * Converts a list of fully qualified annotation names to a comma-separated
+     * string of simple names, each prefixed with the given {@code prefix}.
+     *
+     * <p>Use {@code prefix = "@"} to produce display strings such as
+     * {@code "@AfterBegin"}, or {@code prefix = ""} for plain simple names.
+     *
+     * @param annotations the fully qualified annotation names
+     * @param prefix the string to prepend to each simple name (e.g. {@code "@"})
+     * @return comma-separated simple annotation names with the given prefix
+     */
+    public static String getSimpleAnnotationNames(List<String> annotations, String prefix) {
+        return annotations.stream().map(fq -> prefix + getSimpleName(fq)).distinct().collect(Collectors.joining(", "));
+    }
 }
